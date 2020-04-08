66c3d4722074800ece74641b4699879f2434b6b6
Juan Ignacio <jibarozzibehr@gmail.com>
2020-04-08 04:40:47 -0300

    "Cambios mínimos"

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>¡Hola! Esto es una simple prueba.</Text>
     </View>
   );
 }
