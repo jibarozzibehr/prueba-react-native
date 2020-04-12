57bd7a44b270dc6b94d69f0ec4530a854d1dca92
Juan Ignacio <jibarozzibehr@gmail.com>
2020-04-12 05:23:57 -0300

    Se agregó Card y varias dependencias

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from './components/Card';
+
+
+let Image_Http_URL ={ uri: 'https://reactnativecode.com/wp-content/uploads/2017/05/react_thumb_install.png'};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>¡Hola! Esto es una simple prueba.</Text>
+      <Card
+        title="Hola"
+        caption="Me aburro"
+        subtitle="No tengo sueño"
+        image={{ uri: 'https://images.clarin.com/2019/04/03/vista-del-fitz-roy___TfhNv3KNZ_0x750__1.jpg', }}
+        logo={{ uri: 'https://reactnative.dev/img/tiny_logo.png', }}
+      />
     </View>
   );
 }
